03632dac29e79aea58e9ba11b50da944c56fb14b
森山ナティー <57.moriyama.natee@gmail.com>
2019-09-24 14:12:43 +0900

    design-CarController(スワイプで計算)-2

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     float speed = 0;
+    Vector2 startPos;
     
     void Start()
     {
@@ -14,10 +15,23 @@ public class CarController : MonoBehaviour
     
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))            // マウスがクリックされたら
+        // スワイプの長さがを求める
+        if(Input.GetMouseButtonDown(0))           
         {
-            this.speed = 0.2f;                    // 初速度を設定
+            // マウスをクリックした座標
+            this.startPos = Input.mousePosition;                    
         }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            // マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            // スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 1000.0f;
+
+        }
+
         transform.Translate(this.speed, 0, 0);   // 移動
         this.speed *= 0.98f;                     // 減速
     } 
